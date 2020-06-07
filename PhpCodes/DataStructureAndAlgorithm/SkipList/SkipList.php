80f63ff3e8af1fab2e592227dd2ad5e331ac2ba2
yiliyili <1724335344@qq.com>
2020-06-07 13:03:36 +0800

    跳跃表

--- PhpCodes/DataStructureAndAlgorithm/SkipList/SkipList.php
+++ PhpCodes/DataStructureAndAlgorithm/SkipList/SkipList.php
@@ -36,19 +36,21 @@ class SkipList
 {
     private $maxLevel;
     public $nodePool = [];
-    public $header;
+    public $header;//头结点id值
 
     public function __construct($maxLevel)
     {
-        $this->maxLevel = $maxLevel;
+        $this->maxLevel = $maxLevel;//最大层级
         $header = new Node(-1, $maxLevel);
-        $this->addToNodePool($header->getID(), $header);
+        $this->addToNodePool($header->getID(), $header);//初始化插入
         $this->header = $header->getID();
     }
 
     public function addToNodePool($id, $object)
     {
         $this->nodePool[$id] = $object;
+        // echo'插入';
+        // var_dump($this->nodePool);
     }
 
     /**
@@ -57,6 +59,7 @@ class SkipList
      */
     public function getFromNodePool($id)
     {
+        // print_r($this->nodePool);
         return isset($this->nodePool[$id]) ? $this->nodePool[$id] : null;
     }
 
@@ -66,10 +69,11 @@ class SkipList
 
         $count = 0;
 
-        $tmp = $this->getFromNodePool($this->header);
+        $tmp = $this->getFromNodePool($this->header);//获取node对象
 
         for ($i = $this->maxLevel - 1; $i >= 0; $i--) {
-            while ($tmp && $tmp->forward[$i]) {
+            // var_dump($tmp->forward);//0
+            while ($tmp && $tmp->forward[$i]) {//一开始不进来
                 $count++;
                 if ($count > 20) {
                     break;
@@ -85,14 +89,15 @@ class SkipList
                 }
             }
             if ($tmp) {
-                $visitTrace[$i] = $tmp->getID();
+                $visitTrace[$i] = $tmp->getID();//一开始获取头结点id
             }
         }
 
         $level = $this->randomLevel();
         $newNode = new Node($value, $level);
-        $this->addToNodePool($newNode->getID(), $newNode);
-
+        $this->addToNodePool($newNode->getID(), $newNode);//插入
+// echo $level;
+// var_dump($visitTrace);
         for ($i = 0; $i < $level; $i++) {
             $trace = $this->getFromNodePool($visitTrace[$i]);
             $newNode->forward[$i] = $trace->forward[$i];
@@ -127,13 +132,13 @@ class SkipList
 
         return false;
     }
-
+    //分配一个随机的层数
     private function randomLevel()
     {
         $level = 1;
 
         for ($i = 1; $i < $this->maxLevel; $i++) {
-            if (rand(0, 1)) {
+            if (rand(0, 1)) {//随机产生0挥着1
                 $level++;
             }
         }
@@ -144,28 +149,30 @@ class SkipList
 }
 
 $testData = [898888, 300, 234, 123, 333, 456, 23, 99];
-
+// $testData = [898888];
+echo '<pre>';
 $skipList = new SkipList(3);
 foreach ($testData as $value) {
     $skipList->insert($value);
 }
+ print_r($skipList->nodePool);  //打印的结果看起来有bug,气候看
+// $test = [898888, 300, 234, 123, 333, 456, 23, 99, 100, 111];
+// $test = [898888];
 
-$test = [898888, 300, 234, 123, 333, 456, 23, 99, 100, 111];
+// foreach ($test as $value) {
+//     $result = $skipList->find($value);
+//     if (in_array($value, $testData) && in_array($value, $test)) {
+//         if ($result) {
+//             echo '正确<br>';
+//         } else {
+//             echo '错误<br>';
+//         }
+//     } else {
+//         if ($result) {
+//             echo '错误<br>';
+//         } else {
+//             echo '正确<br>';
+//         }
+//     }
 
-foreach ($test as $value) {
-    $result = $skipList->find($value);
-    if (in_array($value, $testData) && in_array($value, $test)) {
-        if ($result) {
-            echo '正确<br>';
-        } else {
-            echo '错误<br>';
-        }
-    } else {
-        if ($result) {
-            echo '错误<br>';
-        } else {
-            echo '正确<br>';
-        }
-    }
-
-}
+// }
